140207b3fdc6f86a0f5380a7ab2d56e31bde1ab4
YuLinzhong <1427838626@qq.com>
2022-11-29 14:10:25 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <graphics.h>//图形库
+#define WINDOW_LENGHTH 1012
+#define WINDOW_WIDTH 396
+/// <summary>
+/// 游戏初始化
+/// </summary>
+void init() {
+	//创建窗口
+	initgraph(WINDOW_LENGHTH, WINDOW_WIDTH);
+	//游戏背景
+
+}
+int main() {
+	init();//初始化
+}
