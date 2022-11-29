6d61b7b0a69cb3b100d4f0fe49eb32c6811719bd
YuLinzhong <1427838626@qq.com>
2022-11-29 15:43:54 +0800

    实现背景滚动，通过双缓冲解决窗口闪烁

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 #define WINDOW_LENGHTH 1012
 #define WINDOW_WIDTH 396
 
-IMAGE imgbgs[3];//背景全局变量
+IMAGE imgbgs[3];//后中前背景全局变量
 int bgx[3] = { 0 };//背景图片x坐标
+int bgspeed[3] = { 2,3,5 };//后中前背景滚动速度
 
 /// <summary>
 /// 游戏初始化
 /// </summary>
-void init() {
+void init() 
+{
 	//创建窗口
 	initgraph(WINDOW_LENGHTH, WINDOW_WIDTH);
 	//加载背景
@@ -23,16 +25,36 @@ void init() {
 	}
 }
 
-/// <summary>
-/// 渲染游戏背景（前中后景）
-/// </summary>
-void createbackground() {
-	putimagePNG(bgx[0], 0, &imgbgs[0]);
-	putimagePNG(bgx[0], 120, &imgbgs[1]);
-	putimagePNG(bgx[0], 330, &imgbgs[2]);
+/// 游戏背景滚动
+void bgroll()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		bgx[i] -= bgspeed[i];
+		if (bgx[i] < -WINDOW_LENGHTH)	
+			bgx[i] = 0;
+	}
 }
-int main() {
+
+
+/// 渲染游戏背景（前中后景）
+void createbackground() 
+{
+	putimagePNG2(bgx[0], 0, &imgbgs[0]);
+	putimagePNG2(bgx[1], 120, &imgbgs[1]);
+	putimagePNG2(bgx[2], 330, &imgbgs[2]);
+}
+
+int main() 
+{
 	init();//初始化
-	createbackground();//渲染游戏背景
+	while (1)
+	{
+		BeginBatchDraw();//双缓冲
+		createbackground();//渲染游戏背景
+		EndBatchDraw();
+		bgroll();
+		Sleep(10);
+	}
 	system("pause");
 }
